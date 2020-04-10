0c9504348489d3239a6bbe6cdb26faeeae23ec44
hridyesh bisht <41201308+kakabisht@users.noreply.github.com>
2020-04-10 10:44:41 +0530

    Update Java_Project.java
    Code with comments

--- Java_Project.java
+++ Java_Project.java
@@ -519,7 +519,7 @@ public class Java_Project {
     //DES Encryption & Decryption
 
     public static void doCopy(InputStream is, OutputStream os) throws IOException {
-		byte[] bytes = new byte[64];
+		byte[] bytes = new byte[64];//64 bits keys
 		int numBytes;
 		while ((numBytes = is.read(bytes)) != -1) {
 			os.write(bytes, 0, numBytes);
@@ -551,19 +551,19 @@ public class Java_Project {
 
 	public static void encryptOrDecrypt(String key, int mode, InputStream is, OutputStream os) throws Throwable {
 
-		DESKeySpec dks = new DESKeySpec(key.getBytes());
-		SecretKeyFactory skf = SecretKeyFactory.getInstance("DES");
-		SecretKey desKey = skf.generateSecret(dks);
+		DESKeySpec dks = new DESKeySpec(key.getBytes());//encryption keys using java crypto library 
+		SecretKeyFactory skf = SecretKeyFactory.getInstance("DES");//creating Instance 
+		SecretKey desKey = skf.generateSecret(dks);// creating keys 
 		Cipher cipher = Cipher.getInstance("DES"); // DES/ECB/PKCS5Padding for SunJCE
 
 		if (mode == Cipher.ENCRYPT_MODE) {
-			cipher.init(Cipher.ENCRYPT_MODE, desKey);
-			CipherInputStream cis = new CipherInputStream(is, cipher);
-			doCopy(cis, os);
+			cipher.init(Cipher.ENCRYPT_MODE, desKey);//Select Mode = Encrypt
+			CipherInputStream cis = new CipherInputStream(is, cipher);//for inputing data  
+			doCopy(cis, os);//copying the data into file
 		} else if (mode == Cipher.DECRYPT_MODE) {
-			cipher.init(Cipher.DECRYPT_MODE, desKey);
-			CipherOutputStream cos = new CipherOutputStream(os, cipher);
-			doCopy(is, cos);
+			cipher.init(Cipher.DECRYPT_MODE, desKey);// select mode =Decrypt
+			CipherOutputStream cos = new CipherOutputStream(os, cipher);//for outputing data
+			doCopy(is, cos);//copying the data into file
 		}
 	}
 
